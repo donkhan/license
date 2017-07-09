038ec3356818b3109f310976883160cf12c562c3
donkhan <kamilkhan@rediffmail.com>
2017-07-09 22:28:33 +0530

    Made to send back the license information

--- src/main/java/license/LicenseServer.java
+++ src/main/java/license/LicenseServer.java
@@ -7,12 +7,16 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Properties;
+
+import org.json.JSONObject;
 
 public class LicenseServer {
 	private Selector selector;
@@ -20,13 +24,11 @@ public class LicenseServer {
     private OutputStream logStream;
     
     public static void main(String[] args) throws Exception {
-    	System.out.println("MEain Function Called");
     	new LicenseServer("192.168.0.15", 8090).startServer();
     }
 
     public LicenseServer(String address, int port) throws IOException {
     	listenAddress = new InetSocketAddress(address, port);
-    	System.out.println(System.getProperty("user.dir"));
     	File file = new File(System.getProperty("user.dir") + "\\" + "licenseserver.log");
     	if(!file.exists()){
     		file.createNewFile();
@@ -86,7 +88,6 @@ public class LicenseServer {
         Socket socket = channel.socket();
         SocketAddress remoteAddr = socket.getRemoteSocketAddress();
         logStream.write(("Connected to: " + remoteAddr + "\n").getBytes());
-        
         channel.register(this.selector, SelectionKey.OP_READ);
     }
     
@@ -107,9 +108,28 @@ public class LicenseServer {
 
         byte[] data = new byte[numRead];
         System.arraycopy(buffer.array(), 0, data, 0, numRead);
-        buffer.clear();
-        byte [] message = new String("{ product: \"MoneyBox\", company : \"MaxMoney Sdn Bhd.\", noofBranches: \"3\"}").getBytes();
+        String clientContent = new String(data);
+        String licenseContent = process(clientContent);
         
-        channel.write(ByteBuffer.wrap(message));
+        buffer.clear();
+        channel.write(ByteBuffer.wrap(licenseContent.getBytes()));
+    }
+    
+    private String process(String clientContent) throws IOException{
+    	JSONObject json = new JSONObject(clientContent);
+    	String companyName = json.getString("company");
+    	ClassLoader classLoader = getClass().getClassLoader();
+    	URL url = classLoader.getResource(companyName + "license");
+    	if(url == null) {
+    		logStream.write(("License for Company " + companyName + " is not found\n").getBytes());
+    		url = classLoader.getResource("default.license");
+    	}
+    	Properties props = new Properties();
+    	props.load(url.openStream());
+    	JSONObject license = new JSONObject();
+    	license.put("noOfBranches",Integer.parseInt(props.getProperty("noOfBranches")));
+    	String licenseContent = license.toString();
+    	logStream.write((licenseContent+"\n").getBytes());
+    	return licenseContent;
     }
 }
